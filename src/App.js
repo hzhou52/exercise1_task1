b9f5d5b67589c43a3d0c0a69e83a4bfdbdb99647
Hongyuan Zhou <hzhou52@asu.edu>
2019-10-26 20:11:02 -0700

    increment
    increment at home page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import "./App.css";
 import NavBar from "./NavBar";
 import Form from "./Form";
 import ac from "./ac";
-import FormResults from "./FormResults";
+//import FormResults from "./FormResults";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number : 0
+    };
+    this.counter = this.counter.bind(this);
+  }
+  counter() { 
+    var x = this.state.number;
+    this.setState({number : x + 1})
+  };
   render() {
     return (
       <Router>
-        <NavBar />
+        <h1> {this.state.number} 
+        </h1>
+        <NavBar props = {this.counter}/>
         <Switch>
           <Route exact path='/' component={HomeText} />
           <Route path='/form' component={Form} />
           <Route path='/ac' component={ac} />
-          { <Route
+          {/* <Route
             path='/results'
             render={props => <FormResults items={props} {...props} />}
-          ></Route> }
+          ></Route> */}
         </Switch>
       </Router>
     );
--- src/NavBar.js
+++ src/NavBar.js
@@ -3,6 +3,7 @@ import HomeIcon from "@material-ui/icons/Home";
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router-dom";
+import Icon1 from "@material-ui/icons/Build";
 //import AcUnitIcon from '@material-ui/icons/AcUnit';
 import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
 class NavBar extends React.Component {
@@ -22,6 +23,10 @@ class NavBar extends React.Component {
         <Link to='/ac'>
           <DirectionsRunIcon />
         </Link>
+        
+        <button onClick = {this.props.props}>
+          <Icon1 />
+        </button>
 
         {/* <Link to='/yourPathThatMatchesYourRoute'></Link> */}
       </div>
